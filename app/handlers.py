ab4164799ed7f6a0ffd464bda135f70478454f5e
Andrey Parkhomenko <andreiparhomenco@lib.tsu.ru>
2024-11-12 16:59:42 +0700

    1. Добавлены хендлеры фиксации состояния, например для регистрации. 2. Добавлена клавиатура с билдером, чтобы менять клаву автоматически 3.Коллбеки добавлены в инлайн клавиатуру

--- app/handlers.py
+++ app/handlers.py
@@ -3,21 +3,40 @@ from aiogram import (
     Router,
 )  # Импортирую бота, диспетчера, Ф-магический фильтр, благоря нему можно получать любые сообщения от пользователя.
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+
+# импортировали все из файла кейборд
 import app.keyboards as kb
 
-router = (
-    Router()
-)  # из класса айограм достали роутер и подставили его вместо диспетчера dp
+# из файла миддлваре импортируем нашу функцию тестмидлваре
+from app.middlewares import TestMiddleware
+
+# из класса айограм достали роутер и подставили его вместо диспетчера dp
+router = Router()
+# роутер для обработки мидлваре
+router.message.middleware(TestMiddleware())
+
+# из айограм импортировали состояния
+from aiogram.fsm.state import StatesGroup, State
+
+# Импортируем контекст нужен для управления состояниями
+from aiogram.fsm.context import FSMContext
+
+
+# Сделали класс рег в котором будут храниться наши состояния
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 
 # Получаем от пользователя /start и отправляем ему в ответ первое сообщение
+# Метод реплай это именно ответ на отправленное сообщение, т.е в содержании будет указано первое сообщение, в отличии от ансвер
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    # Метод реплай это именно ответ на отправленное сообщение, т.е в содержании будет указано первое сообщение, в отличии от ансвер
+
     await message.reply(
         f"Привет! Я ответил!\nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}",
-        reply_markup=kb.settings,
+        reply_markup=kb.main,
     )
 
 
@@ -46,3 +65,39 @@ async def get_photo(message: Message):
 @router.message(F.photo)
 async def take_photo(message: Message):
     await message.answer(f"ID фото: {message.photo[-1].file_id}")
+
+
+# Получаем callback от кейбордс
+@router.callback_query(F.data == "firstcourse")
+async def firstcourse(callback: CallbackQuery):
+    await callback.answer("Вы выбрали первокурсникам", show_alert=True)
+    await callback.message.edit_text(
+        "Погрузим тебя в атмосферу библиотеки в твои первые дни учебы",
+        reply_markup=await kb.Inline_roles(),
+    )
+
+
+# Делаем роутер для фиксации регистрации через состояния, шаг первый запрашиваем при регистрации имя
+@router.message(Command("reg"))
+async def reg_one(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer("Vvedite imya")
+
+
+# Делаем шаг второй сохраняем имя и запрашиваем номер
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer("Введите номер телефона")
+
+
+# Делаем шаг третий записываем в переменную дата информацию из переменных и показываем пользователю, отчистили состояния
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(
+        f"thank, register is done \n Имя: {data['name']}\n Номер:{data['number']}"
+    )
+    await state.clear()
--- app/keyboards.py
+++ app/keyboards.py
@@ -5,16 +5,32 @@ from aiogram.types import (
     InlineKeyboardButton,
 )
 
+# Ипортируем билдер, который может подставлять значения в клавиатуру, переданные из вне, или предопределенные
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
 # создаем кнопки клавитуры с помошью реплай кейборд
-# Меняем размер клавиатуры до минимального значения
-# текст вместо  подсказки значения(write a message)
-main = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Первокурсникам"), KeyboardButton(text="Режим работы")],
-        [KeyboardButton(text="Получить читательский")],
-    ],
-    resize_keyboard=True,
-    input_field_placeholder="Выберите пункт меню",
+# resize_keyboard=True Меняем размер клавиатуры до минимального значения
+# input_field_placeholder вместо  подсказки значения(write a message)
+# main = ReplyKeyboardMarkup(
+#    keyboard=[
+#       [KeyboardButton(text="Первокурсникам"), KeyboardButton(text="Режим работы")],
+#       [KeyboardButton(text="Получить читательский")],
+#   ],
+#   resize_keyboard=True,
+#   input_field_placeholder="Выберите пункт меню",
+# )
+
+main = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Первокурсникам", callback_data="firstcourse")],
+        [
+            InlineKeyboardButton(text="Режим работы", callback_data="worktime"),
+            InlineKeyboardButton(
+                text="Получить читательский", callback_data="get_reader_cart"
+            ),
+        ],
+    ]
 )
 
 settings = InlineKeyboardMarkup(
@@ -27,3 +43,23 @@ settings = InlineKeyboardMarkup(
         ]
     ]
 )
+
+# значения билдера
+roles = ["Сотрудник ТГУ", "Студент ТГУ", "Сторонний пользователь"]
+
+
+async def Reply_roles():
+    keyboard = ReplyKeyboardBuilder()
+    for role in roles:
+        keyboard.add(KeyboardButton(text=role))
+    return keyboard.adjust(2).as_markup()
+
+
+async def Inline_roles():
+    keyboard = InlineKeyboardBuilder()
+    for role in roles:
+        keyboard.add(InlineKeyboardButton(text=role, callback_data=f"role_{role}"))
+    return keyboard.adjust(2).as_markup()
+
+
+# as_markup use always
