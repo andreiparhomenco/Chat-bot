eb54770a0c33e93dac942113b5101e4c60f75a4a
Andrey Parkhomenko <andreiparhomenco@lib.tsu.ru>
2024-11-28 17:48:25 +0700

    Создал: блок первокурсникам режим работы Функцию получения читательского Частично активности

--- app/keyboards.py
+++ app/keyboards.py
@@ -24,7 +24,7 @@ main = ReplyKeyboardMarkup(
     resize_keyboard=True,
     input_field_placeholder="Выберите пункт меню",
 )
-
+# Клавиатура первокурсникам
 firstcourse = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="График выдачи учебной литературы")],
@@ -32,11 +32,57 @@ firstcourse = ReplyKeyboardMarkup(
         [KeyboardButton(text='Мобильное приложение "Научка"')],
         [KeyboardButton(text="Книжная полка первокурсника")],
         [KeyboardButton(text="Получить читательский")],
-        [KeyboardButton(text="Назад")],
+        [KeyboardButton(text="Меню")],
     ],
     resize_keyboard=True,
     input_field_placeholder="Выберите пункт меню",
 )
+# Клавиатура получить читательский
+get_reader_card = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Студент или сотрудник ТГУ")],
+        [KeyboardButton(text="Студент или сотрудник ВУЗов Томска")],
+        [KeyboardButton(text="Сторонний пользователь")],
+        [KeyboardButton(text="Меню")],
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите пункт меню",
+)
+# Клавиатура активности
+activnosti = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="VR выставки"), KeyboardButton(text="Экскурсии")],
+        [KeyboardButton(text="Квесты"), KeyboardButton(text="Стать волонтером")],
+        [KeyboardButton(text="Факты о научке"), KeyboardButton(text="Настолки")],
+        [KeyboardButton(text="Языковые клубы"), KeyboardButton(text="Подкасты")],
+        [KeyboardButton(text="Фотосессия в библиотеке")],
+        [KeyboardButton(text="Меню")],
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите пункт меню",
+)
+
+kvesty = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='"В плену книжных стеллажей"')],
+        [KeyboardButton(text='"Загадки города Томска"')],
+        [KeyboardButton(text="Меню")],
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите квест",
+)
+
+podkast = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Как это было')],
+        [KeyboardButton(text='Библиомаршрут')],
+        [KeyboardButton(text='Фронтиспис: Кабинет Пушкинского')],
+        [KeyboardButton(text="Меню")],
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите квест",
+)
+
 # клавиатура для инлайнов
 """ main = InlineKeyboardMarkup(
     inline_keyboard=[
